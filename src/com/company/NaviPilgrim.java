9d58a329dd509aca0d76071a0e5129f80f95e06e
Павел Климов <klimovpavel62@gmail.com>
2022-04-14 16:33:08 +0300

    firs

---
+++ src/com/company/NaviPilgrim.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.io.*;
+import java.net.*;
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class NaviPilgrim {
+
+    public static void main(String[] args) {
+
+        int port = 5500;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            System.out.println("Server is listening on port " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+
+
+                String text;
+                do {
+                    text = reader.readLine();
+                    if (text != null) {
+                        System.out.println(text);
+                    }
+
+                } while (text != null);
+
+                socket.close();
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
